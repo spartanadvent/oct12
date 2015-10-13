bdfa4f48a2c1c32f3c3afefb0219ef9f293e32e2
spartanadvetus <geekwise.eric.cesena@gmail.com>
2015-10-13 22:03:44 +0000

    updated

---
+++ js/app.js
@@ -0,0 +1,61 @@
+var github_token = "fbb66b946b2dd5ea55578885173043b7e42c7eaa"
+
+var github_connection;
+var repo;
+
+function repos(repo_in_use){
+    
+  return  repo = github_connection.getRepo('spartanadvent', repo_in_use);
+    
+}
+
+function load_javascript(script_url,function_to_run_onload){
+
+    var document_head = document.getElementsByTagName('head')[0];
+    var document_script = document.createElement('script');
+    
+    document_script.type = 'text/javascript';
+    document_script.src = script_url;
+    document_script.onready = function_to_run_onload;
+    document_script.addEventListener('onload',function(event){
+        function_to_run_onload;
+    });
+    
+    document_head.appendChild(document_script);
+
+}
+function log(message){
+
+  console.log(message);
+  
+}
+
+
+document.addEventListener('DOMContentLoaded', function(){
+
+  
+  log(Date.now());
+     log('DOMContnetLoaded');
+    
+    load_javascript('js/github_api.js',function(event){
+        
+    });
+
+  
+
+});
+
+
+document.addEventListener('readystatechange',function(event){
+    
+    if(document.readyState === 'complete'){
+        
+        github_connection = new Github({ 
+              token: github_token,
+              auth: "oauth"
+            });
+    
+    
+    }
+    
+});
